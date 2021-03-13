16e4ef473151d92c089c52fec08af3794bd448b7
GuiCoelho <guiminervino@gmail.com>
2021-03-13 08:09:11 -0300

    do not push on master

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
-
-#include <iostream>
-
+#include "../include/types.hpp"
 int main(int argc, char* argv[]) {
 
+ 
 
+  if (argc != 3) {
+    std::cout 
+      << "Comando incorreto!\n"
+      << "Comando leitor/exibidor: -e arquivo.class\n";
+  } else {
+    std::string command = argv[1], filename = argv[2];
 
-  std::cout << "\n\nGui_Branch!";
+    // leitor/exibidor
+    if(command == "-e") {
+      std::cout << "Modo leitor e exibidor!" << std::endl;
+
+    }
+  }
+
+  std::cout << "\n\nTerminou execucao";
   return 0;
 }
