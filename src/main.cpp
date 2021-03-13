974c8a2dcdc5742490d9d8ec5fc821bac2962bdc
GuiCoelho <guiminervino@gmail.com>
2021-03-13 07:40:58 -0300

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
-#include "file_reader.hpp"
+
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 
-  Class_File_Format class_file;
 
-  if (argc != 3) {
-    std::cout 
-      << "Comando incorreto!\n"
-      << "Comando leitor/exibidor: -e arquivo.class\n";
-  } else {
-    std::string command = argv[1], filename = argv[2];
-
-    // leitor/exibidor
-    if(command == "-e") {
-      class_file = read_class_file(filename);
-
-    }
-  }
 
   std::cout << "\n\nTerminou execucao";
   return 0;
