77f960e01a64184045ca118633d184fd8de77681
GuiCoelho <guiminervino@gmail.com>
2021-03-13 07:38:08 -0300

    maintain

--- src/interface_info.cpp
+++
@@ -1,6 +0,0 @@
-#include "file_reader.hpp"
-
-void read_interface_info(FILE *file, Class_File_Format *class_file) {
-  for (int i = 0; i < class_file->interfaces_count; i++)
-    class_file->interfaces[i].interface_table = read_2_bytes(file);
-}
