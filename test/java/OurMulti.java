4a8637ee2dd3678f3334325df4c9e10b69f466cd
GuiCoelho <guiminervino@gmail.com>
2021-04-21 17:59:20 -0300

    Comentários em OurMulti

--- test/java/OurMulti.java
+++ test/java/OurMulti.java
@@ -1,37 +1,41 @@
 public class OurMulti {
   public static void main(String[] args) {
+    
+    
+//NOTE: Esse caso NÃO passa
 
     // Integer[][] arrayOfString = new Integer[10][10];
-    Integer[] arrayOfString = new Integer[10];
-
-
-    // arrayOfString[0][0] = "String1";
-    // arrayOfString[0][1] = "String2";
-    // arrayOfString[0][2] = "String3";
-    // arrayOfString[9][5] = "String4";
 
     // arrayOfString[0][0] = 1;
     // arrayOfString[0][1] = 2;
     // arrayOfString[0][2] = 3;
     // arrayOfString[9][5] = 4;
 
+    // System.out.println(arrayOfString[0][0]);
+    // System.out.println(arrayOfString[1][0]);
+
+//NOTE: Esse caso NÃO passa
+
+    // Integer[] arrayOfString = new Integer[10];
+
+    // arrayOfString[0] = 1;
+    // arrayOfString[1] = 2;
+    // arrayOfString[2] = 3;
+    // arrayOfString[5] = 4;
+
+    // System.out.println(arrayOfString[0]);
+    // System.out.println(arrayOfString[1]);
 
 
-    arrayOfString[0] = 1;
-    arrayOfString[1] = 2;
-    arrayOfString[2] = 3;
-    arrayOfString[5] = 4;
-
-    System.out.println(arrayOfString[0]);
-    System.out.println(arrayOfString[1]);
-    System.out.println(arrayOfString[2]);
-    System.out.println(arrayOfString[5]);
+//NOTE: Esse caso passa
 
     // int[] arrayOfString = new int[3];
     // arrayOfString[0] = 1;
     // arrayOfString[1] = 2;
     // System.out.println(arrayOfString[0]);
     // System.out.println(arrayOfString[1]);
+
+//NOTE: Esse caso passa
 
       // int[][] arrayOfString = new int[3][3];
     // arrayOfString[0][2] = 1;
