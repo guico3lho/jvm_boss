41126f2fed3ad79210da9729197f96aacb19f846
GuiCoelho <guiminervino@gmail.com>
2021-04-21 17:08:22 -0300

    a

--- test/java/OurMulti.java
+++ test/java/OurMulti.java
@@ -2,7 +2,7 @@ public class OurMulti {
   public static void main(String[] args) {
 
     // Integer[][] arrayOfString = new Integer[10][10];
-    // Integer[] arrayOfString = new Integer[10];
+    Integer[] arrayOfString = new Integer[10];
 
 
     // arrayOfString[0][0] = "String1";
@@ -15,10 +15,10 @@ public class OurMulti {
     // arrayOfString[0][2] = 3;
     // arrayOfString[9][5] = 4;
 
-    // arrayOfString[0] = 1;
-    // arrayOfString[1] = 2;
-    // arrayOfString[2] = 3;
-    // arrayOfString[5] = 4;
+    arrayOfString[0] = 1;
+    arrayOfString[1] = 2;
+    arrayOfString[2] = 3;
+    arrayOfString[5] = 4;
 
     System.out.println(arrayOfString[0]);
     System.out.println(arrayOfString[1]);
