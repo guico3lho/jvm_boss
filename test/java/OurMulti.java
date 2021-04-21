9bab605cea5e96efe351d1fec612a2116f15e981
GuiCoelho <guiminervino@gmail.com>
2021-04-21 16:15:09 -0300

    Multiplicacao OurMulti.java funcionando

---
+++ test/java/OurMulti.java
@@ -0,0 +1,10 @@
+public class OurMulti {
+    public static void main(String[] args) {
+     
+      int x = 12;
+      int y = 2;
+      int z = x*y;
+      System.out.println("Multiplication: " + z);
+     
+    }
+  }
