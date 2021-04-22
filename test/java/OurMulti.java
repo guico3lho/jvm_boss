8bf4785abb580b09d108fea6d1bc1377de4434aa
GuiCoelho <guiminervino@gmail.com>
2021-04-22 11:39:20 -0300

    Alguns testes em OurMulti

--- test/java/OurMulti.java
+++ test/java/OurMulti.java
@@ -27,21 +27,21 @@ public class OurMulti {
 
 //NOTE: Esse caso passa
 
-    // int[] arrayOfString = new int[3];
-    // arrayOfString[0] = 1;
-    // arrayOfString[1] = 2;
-    // System.out.println(arrayOfString[0]);
-    // System.out.println(arrayOfString[1]);
+    int[] arrayOfString = new int[10];
+    arrayOfString[0] = 1;
+    arrayOfString[9] = 2;
+    System.out.println(arrayOfString[0]);
+    System.out.println(arrayOfString[9]);
 
 //NOTE: Esse caso NAO passa
 
-    int[][] arrayOfString = new int[3][3];
-    arrayOfString[0][2] = 1;
-    arrayOfString[1][2] = 2;
-    arrayOfString[2][2] = 10;
-    System.out.println(arrayOfString[0][2]);
-    System.out.println(arrayOfString[1][2]);
-    System.out.println(arrayOfString[2][2]);
+    // int[][] arrayOfString = new int[3][3];
+    // arrayOfString[0][2] = 1;
+    // arrayOfString[1][2] = 2;
+    // arrayOfString[2][2] = 10;
+    // System.out.println(arrayOfString[0][2]);
+    // System.out.println(arrayOfString[1][2]);
+    // System.out.println(arrayOfString[2][2]);
 
   }
 }
