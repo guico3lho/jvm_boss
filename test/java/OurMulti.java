fd7838ac26063ad8362eef7874d13078f7dc3ad8
GuiCoelho <guiminervino@gmail.com>
2021-04-25 15:58:55 -0300

    feat (OurMulti) adiciona um caso

--- test/java/OurMulti.java
+++ test/java/OurMulti.java
@@ -27,21 +27,31 @@ public class OurMulti {
 
 //NOTE: Esse caso passa
 
-  String[] arrayOfString = new String[10];
-  arrayOfString[0] = "String 1";
-  arrayOfString[9] = "String 2";
-  System.out.println(arrayOfString[0]);
-  System.out.println(arrayOfString[9]);
+  // String[] arrayOfString = new String[10];
+  // arrayOfString[0] = "String 1";
+  // arrayOfString[9] = "String 2";
+  // System.out.println(arrayOfString[0]);
+  // System.out.println(arrayOfString[9]);
 
 //NOTE: Esse caso NAO passa
 
-    // int[][] arrayOfString = new int[3][3];
-    // arrayOfString[0][2] = 1;
-    // arrayOfString[1][2] = 2;
-    // arrayOfString[2][2] = 10;
-    // System.out.println(arrayOfString[0][2]);
-    // System.out.println(arrayOfString[1][2]);
-    // System.out.println(arrayOfString[2][2]);
+    int[][] arrayOfString = new int[3][3];
+    arrayOfString[0][2] = 1;
+    arrayOfString[1][2] = 2;
+    arrayOfString[2][2] = 10;
+    System.out.println(arrayOfString[0][2]);
+    System.out.println(arrayOfString[1][2]);
+    System.out.println(arrayOfString[2][2]);
+
+    //NOTE: Esse caso passa
+
+    // int[] arrayOfString = new int[3];
+    // arrayOfString[0] = 1;
+    // arrayOfString[1] = 2;
+    // arrayOfString[2] = 10;
+    // System.out.println(arrayOfString[0]);
+    // System.out.println(arrayOfString[1]);
+    // System.out.println(arrayOfString[2]);
 
   }
 }
