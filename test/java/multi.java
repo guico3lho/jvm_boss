35722089829c54279cac7bad4a1f3c6729c46872
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-26 06:18:15 -0300

    feat (multianewarray): v2

--- test/java/multi.java
+++ test/java/multi.java
@@ -1,24 +1,34 @@
 class multi {
   public static void main(String[] paramArrayOfString) {
 
-    int[][][] arrayOfInt = new int[2][3][4];
+    int[][] arrayOfInt = new int[2][3];
 
-    arrayOfInt[0][0][0] = 1;
-    arrayOfInt[0][1][0] = 2;
-    arrayOfInt[1][1][1] = 3;
+    arrayOfInt[0][0] = 1;
+    arrayOfInt[0][1] = 2;
+    arrayOfInt[1][1] = 3;
 
-    System.out.println(arrayOfInt[0][0][0]);
-    System.out.println(arrayOfInt[0][1][0]);
-    System.out.println(arrayOfInt[1][1][1]);
+    System.out.println(arrayOfInt[0][0]);
+    System.out.println(arrayOfInt[0][1]);
+    System.out.println(arrayOfInt[1][1]);
 
-    // String[][][] arrayOfString = new String[2][3][4];
+    int[][][] arrayOfInt2 = new int[2][3][4];
 
-    // arrayOfString[0][0][0] = "teste";
-    // arrayOfString[0][1][0] = "teste2";
-    // arrayOfString[1][1][1] = "teste4";
+    arrayOfInt2[0][0][0] = 1;
+    arrayOfInt2[0][1][0] = 2;
+    arrayOfInt2[1][1][1] = 3;
 
-    // System.out.println(arrayOfString[0][0][0]);
-    // System.out.println(arrayOfString[0][1][0]);
-    // System.out.println(arrayOfString[1][1][1]);
+    System.out.println(arrayOfInt2[0][0][0]);
+    System.out.println(arrayOfInt2[0][1][0]);
+    System.out.println(arrayOfInt2[1][1][1]);
+
+    String[][][] arrayOfString = new String[2][3][4];
+
+    arrayOfString[0][0][0] = "teste";
+    arrayOfString[0][1][0] = "teste2";
+    arrayOfString[1][1][1] = "teste4";
+
+    System.out.println(arrayOfString[0][0][0]);
+    System.out.println(arrayOfString[0][1][0]);
+    System.out.println(arrayOfString[1][1][1]);
   }
 }
