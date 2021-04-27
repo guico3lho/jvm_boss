c088fb2ea4d5a828c3fd7a124698dda92a930011
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-26 21:35:55 -0300

    remove some java files

--- test/java/_poupanca.java
+++
@@ -1,9 +0,0 @@
-class Poupanca extends _conta {
-    Poupanca(double paramDouble) {
-      super(paramDouble);
-    }
-    
-    void atualizacaoMensal() {
-      creditar(obterSaldo() * 0.005D);
-    }
-  }
